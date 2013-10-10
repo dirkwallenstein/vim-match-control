dirkwallenstein/vim-match-control

commit c77614928dcaeb1beb577b498d01948d1d9517d6

Work around sporadic catch failure
I've had some E716 errors slipping through in combination with using
makegreen (more precisely the plugin formerly known as makegreen).
I guess this is more failsafe now

plugin/match-control.vim
@@ -198,21 +198,22 @@ fun s:MatchControl._GetMatchSpecs(mode) dict
     " Return the list of match-specs for the given a:mode.  Valid modes are
     " 'permanent', 'insert' and 'normal'.
     let l:match_setup = self._GetMatchSetup()
-    if !empty(&ft)
-        try
-            let l:ft_dict = l:match_setup[&ft]
-            let l:ft_match_specs = l:ft_dict[a:mode]
-            return l:ft_match_specs
-        catch /E716/ " Key not present in Dictionary
-        endtry
+    let l:found_match_specs = []
+    " search ft-specific entry
+    if !empty(&ft) && has_key(l:match_setup, &ft)
+        let l:ft_dict = l:match_setup[&ft]
+        if has_key(l:ft_dict, a:mode)
+            let l:found_match_specs = l:ft_dict[a:mode]
+        endif
     endif
-    try
+    " search fallback entry '*'
+    if empty(l:found_match_specs) && has_key(l:match_setup, '*')
         let l:default_dict = l:match_setup['*']
-        let l:fallback_match_specs = l:default_dict[a:mode]
-        return l:fallback_match_specs
-    catch /E716/ " Key not present in Dictionary
-    endtry
-    return []
+        if has_key(l:default_dict, a:mode)
+            let l:found_match_specs = l:default_dict[a:mode]
+        endif
+    endif
+    return l:found_match_specs
 endfun
 
 "

commit 6853e6819ec0c70ae6e6ea41f7a8afd5baf9b1a0

Restore ability to override a fallback mode with an empty list
It got broken in c77614928dcaeb1beb577b498d01948d1d9517d6

You can now again override the fallback match-specification completely
with a filetype specific entry.  Checking an empty list (the default
return value if nothing is found) did of course not prevent a lookup in
the fallback record.

plugin/match-control.vim
@@ -214,6 +214,7 @@ fun s:MatchControl._GetMatchSpecs(mode) dict
     " 'permanent', 'insert' and 'normal'.
     let l:match_setup = self._GetMatchSetup()
     let l:found_match_specs = []
+    let l:found_ft_entry_for_mode = 0
     " search ft-specific entry.  Use '!' as the key for empty &ft because
     " a dictionary cannot have an empty key.
     let l:ft_key = empty(&ft) ? '!' : &ft
@@ -221,10 +222,11 @@ fun s:MatchControl._GetMatchSpecs(mode) dict
         let l:ft_dict = l:match_setup[l:ft_key]
         if has_key(l:ft_dict, a:mode)
             let l:found_match_specs = l:ft_dict[a:mode]
+            let l:found_ft_entry_for_mode = 1
         endif
     endif
     " search fallback entry '*'
-    if empty(l:found_match_specs) && has_key(l:match_setup, '*')
+    if !l:found_ft_entry_for_mode && has_key(l:match_setup, '*')
         let l:default_dict = l:match_setup['*']
         if has_key(l:default_dict, a:mode)
             let l:found_match_specs = l:default_dict[a:mode]